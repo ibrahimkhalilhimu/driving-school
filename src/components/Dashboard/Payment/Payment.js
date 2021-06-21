b159c55797b81506cc3c358c8a52ef921812a130
ibrahimkhalilhemu <ibrahimkhalilhemu1@gmail.com>
2021-06-21 21:57:28 +0600

    splitFrom add

--- src/components/Dashboard/Payment/Payment.js
+++ src/components/Dashboard/Payment/Payment.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
 import SimpleCardFrom from './SimpleCardFrom';
+import SplitForm from './SplitForm';
 const stripePromise = loadStripe('pk_test_51HZr9GLVoFmXs1X2Ur8De4DlFcFzKmWBjHxbjF91XhSgAiO6ujsEVoVfpbnvihNRIDlsKZPymKGUvSjxeEHmDWoQ00NxaeEbFZ');
 
 const Payment = ({handlePayment}) => {
     return (
         <Elements stripe={stripePromise}>
-  <SimpleCardFrom handlePayment={handlePayment}/>
+  {/* <SimpleCardFrom handlePayment={handlePayment}/> */}
+  <SplitForm handlePayment={handlePayment}/>
     </Elements>
     );
 };
---
+++ src/components/Dashboard/Payment/SplitForm.js
@@ -0,0 +1,135 @@
+import React, { useMemo } from "react";
+import swal from 'sweetalert'
+import './SplitForm.css'
+import {
+  useStripe,
+  useElements,
+  CardNumberElement,
+  CardCvcElement,
+  CardExpiryElement
+} from "@stripe/react-stripe-js";
+
+import useResponsiveFontSize from "./useResponsiveFontSize";
+
+const useOptions = () => {
+  const fontSize = useResponsiveFontSize();
+  const options = useMemo(
+    () => ({
+      style: {
+        base: {
+          fontSize,
+          color: "#424770",
+          letterSpacing: "0.025em",
+          fontFamily: "Source Code Pro, monospace",
+          "::placeholder": {
+            color: "#aab7c4"
+          }
+        },
+        invalid: {
+          color: "#9e2146"
+        }
+      }
+    }),
+    [fontSize]
+  );
+
+  return options;
+};
+
+const SplitForm = ({handlePayment}) => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const options = useOptions();
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js has not loaded yet. Make sure to disable
+      // form submission until Stripe.js has loaded.
+      return;
+    }
+
+    const payload = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardNumberElement)
+    });
+    console.log("[PaymentMethod]", payload);
+    if (payload.error) {
+        swal("Failed!", payload.error.message, "error", { dangerMode: true });
+      } else {
+        swal("Payment Successfully", "Your Payment Successfully!", "success");
+        handlePayment(payload.id)
+      }
+  };
+
+  return (
+  <div className="splitFrom">
+        <form onSubmit={handleSubmit} >
+      <label >
+        Card number
+        <CardNumberElement
+       
+          options={options}
+          onReady={() => {
+            console.log("CardNumberElement [ready]");
+          }}
+          onChange={event => {
+            console.log("CardNumberElement [change]", event);
+          }}
+          onBlur={() => {
+            console.log("CardNumberElement [blur]");
+          }}
+          onFocus={() => {
+            console.log("CardNumberElement [focus]");
+          }}
+        />
+      </label>
+      <br/>
+      <label>
+        Expiration date
+        <CardExpiryElement
+          options={options}
+          onReady={() => {
+            console.log("CardNumberElement [ready]");
+          }}
+          onChange={event => {
+            console.log("CardNumberElement [change]", event);
+          }}
+          onBlur={() => {
+            console.log("CardNumberElement [blur]");
+          }}
+          onFocus={() => {
+            console.log("CardNumberElement [focus]");
+          }}
+        />
+      </label>
+      <br/>
+      <label >
+        CVC
+        <CardCvcElement
+          options={options}
+          onReady={() => {
+            console.log("CardNumberElement [ready]");
+          }}
+          onChange={event => {
+            console.log("CardNumberElement [change]", event);
+          }}
+          onBlur={() => {
+            console.log("CardNumberElement [blur]");
+          }}
+          onFocus={() => {
+            console.log("CardNumberElement [focus]");
+          }}
+        />
+      </label>
+      <br/>
+      <button className="btn btn-danger" type="submit" disabled={!stripe}>
+        Payment
+      </button>
+    </form>
+  </div>
+  );
+};
+
+export default SplitForm;
